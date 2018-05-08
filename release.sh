447697cd467e380b6a976ed66011d82eca383024
Marlon Saglia <esolitos@gmail.com>
2018-05-08 15:57:11 +0200

    Add LATEST_TAG

--- release.sh
+++ release.sh
@@ -5,15 +5,7 @@ set -e
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" && ("${TRAVIS_BRANCH}" == "master"  || -n "${TRAVIS_TAG}") ]]; then
   docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
-  if [[ -n "${TRAVIS_TAG}" ]]; then
-    export STABILITY_TAG="${TRAVIS_TAG}"
-  fi
-
   make release
-
-  if [[ -n "${EXTRA_TAG}" ]]; then
-    make release TAG="${EXTRA_TAG}"
-  fi
 
   if [[ "${TAG}" == "${LATEST_TAG}" ]]; then
     make release TAG="latest"
